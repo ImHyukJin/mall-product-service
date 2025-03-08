8b209ddd1ef7d3a43be2c90cbdb8b5c4b8ced5ae
ImHyukJin <ohj9575@gmail.com>
2025-03-09 05:35:12 +0900

    feat:product first deploy

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 # 기본 이미지로 OpenJDK 사용
 FROM openjdk:11-jdk-slim
 
-# 작업 디렉토리 설정
-WORKDIR /app
+ARG JAR_FILE=build/libs/*.jar
+ARG PROFILES
+ARG ENV
 
 # 빌드된 JAR 파일을 컨테이너에 복사
-COPY build/libs/MALL_PRODUCT_SERVICE-0.0.1-SNAPSHOT.jar /app/MALL_PRODUCT_SERVICE-0.0.1-SNAPSHOT.jar
+COPY ${JAR_FILE} app.jar
 
 # Spring Boot 애플리케이션 실행
-ENTRYPOINT ["java", "-jar", "/app/MALL_PRODUCT_SERVICE-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=${PROFILES}", "-Dserver.env=${ENV}","-jar","app.jar"]
 
 # 컨테이너가 외부와 통신할 포트 설정
-EXPOSE 8081
+# EXPOSE 8081
--- src/main/java/com/dmall/productservice/controller/ProductController.java
+++ src/main/java/com/dmall/productservice/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.dmall.productservice.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +18,37 @@ import com.dmall.productservice.model.Product;
 import com.dmall.productservice.repository.ProductRepository;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/product")
 public class ProductController {
 
+	@Value("${server.env}")
+	private String env;
+	@Value("${server.port}")
+	private String serverPort;
+	@Value("${server.address}")
+	private String serverAddress;
+	@Value("${serverName}")
+	private String serverName;
+	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	//서버가 잘 돌아가나
+	@GetMapping("/hc")
+	public ResponseEntity<?> healthCheck() {
+		Map<String, String> responseData = new TreeMap<>();
+		responseData.put("env", env);
+		responseData.put("serverPort", serverPort);
+		responseData.put("serverAddress", serverAddress);
+		responseData.put("serverName", serverName);
+		
+		return ResponseEntity.ok(responseData);
+	}
+	
+	@GetMapping("/env")
+	public ResponseEntity<?> getEnv() {
+		return ResponseEntity.ok(env);
+	}
 	
 	//전체 상품 목록 조회
 	@GetMapping("/")

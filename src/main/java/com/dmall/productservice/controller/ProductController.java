1668d48cb0e425e3319496907f83bfee21ff1b50
ImHyukJin <ohj9575@gmail.com>
2025-03-12 15:06:00 +0900

    test: index.html 화면

--- src/main/java/com/dmall/productservice/controller/ProductController.java
+++ src/main/java/com/dmall/productservice/controller/ProductController.java
@@ -7,7 +7,9 @@ import java.util.TreeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,15 +28,18 @@ public class ProductController {
 	private ProductRepository productRepository;
 
 	
-	//전체 상품 목록 조회
-	@GetMapping("/")
-	public List<Product> getAllProducts() {
-		return productRepository.findAll();
-	}
-	
-	//상품 추가 (post)
-	@PostMapping("/")
-	public Product addProduct(@RequestBody Product product) {
-		return productRepository.save(product);
-	}
+	// 전체 상품 목록 조회
+    @GetMapping("/")
+    public String getAllProducts(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "index"; // templates/index.html을 렌더링
+    }
+
+    // 상품 추가 (POST)
+    @PostMapping("/")
+    public String addProduct(@ModelAttribute Product product) {
+        productRepository.save(product);
+        return "redirect:/"; // 상품 추가 후 목록 페이지로 리다이렉트
+    }
 }
